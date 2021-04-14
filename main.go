0b45c7d9d13ccffc2f8d13ca873715b770dc53a5
Ilya Levin <my.900913.acc0unt@gmail.com>
2021-04-14 15:37:48 +0300

    writer function

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"database/sql"
 	"fmt"
 	"log"
+	"strconv"
 	"time"
 
 	_ "github.com/go-sql-driver/mysql"
@@ -25,12 +26,16 @@ func dsn(db string) string {
 	return fmt.Sprintf("%s:%s@tcp(%s)/%s", Username, Password, Hostname, db)
 }
 
+func createDatabaseSQL() string {
+	return fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s;", DatabaseName)
+}
+
 func createTableSQL() string {
 	return fmt.Sprintf("CREATE TABLE IF NOT EXISTS %s (%s INT PRIMARY KEY AUTO_INCREMENT, %s VARCHAR(255) NOT NULL);", Table, IdField, DataField)
 }
 
-func createDatabaseSQL() string {
-	return fmt.Sprintf("CREATE DATABASE IF NOT EXISTS %s;", DatabaseName)
+func insertSQL(data string) string {
+	return fmt.Sprintf("INSERT INTO %s(%s) VALUES (%s);", Table, DataField, data)
 }
 
 func createTable() error {
@@ -59,8 +64,8 @@ func createTable() error {
 	return nil
 }
 
-func createDB() error {
-	db, err := sql.Open("mysql", dsn(""))
+func createDatabase() error {
+	db, err := sql.Open(Driver, dsn(""))
 	if err != nil {
 		return fmt.Errorf("error [%s] when opening DB\n", err)
 	}
@@ -85,8 +90,31 @@ func createDB() error {
 	return nil
 }
 
+func writer(pattern string, n int) error {
+	db, err := sql.Open(Driver, dsn(DatabaseName))
+	if err != nil {
+		return fmt.Errorf("error [%s] when opening DB\n", err)
+	}
+	defer func(db *sql.DB) {
+		err := db.Close()
+		if err != nil {
+			log.Printf("error [%s] when closing DB\n", err)
+		}
+	}(db)
+
+	ctx, cancel := context.WithTimeout(context.Background(), time.Duration(n) * time.Second)
+	defer cancel()
+	for i := 0; i < n; i++ {
+		_, err := db.ExecContext(ctx, insertSQL(strconv.Itoa(i)))
+		if err != nil {
+			return fmt.Errorf("error [%s] when creating table\n", err)
+		}
+	}
+	return nil
+}
+
 func main() {
-	err := createTable()
+	err := writer("", 2000)
 	if err != nil {
 		log.Fatal(err)
 	}
